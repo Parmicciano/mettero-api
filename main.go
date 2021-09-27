07ce74bab28ca99b6a439908d8ccdba6c388f009
Parmicciano <85128225+Parmicciano@users.noreply.github.com>
2021-09-27 22:14:38 +0200

    commit

--- main.go
+++ main.go
@@ -3,47 +3,51 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
-	"net/http"
-	"github.com/mcnijman/go-emailaddress"
 	"log"
-    "github.com/PuerkitoBio/goquery"
+	"net/http"
+	"strings"
+
+	"github.com/PuerkitoBio/goquery"
+	"github.com/mcnijman/go-emailaddress"
 )
 
 func main() {
-	url := "https://amazon.fr"
-	fmt.Printf("HTML code of %s ...\n", url)
-	resp, err := http.Get(url)
-	// handle the error if there is one
-	if err != nil {
-		panic(err)
-	}
-	// do this now so it won't be forgotten
-	defer resp.Body.Close()
-	// reads html as a slice of bytes
-	html, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		panic(err)
-	}
-	// show the HTML code as a string %s
-	souphtml := fmt.Sprintf("%s\n", html)
-	
+	LINKS := []string{}
 
+	doc, err := goquery.NewDocument("https://apple.com")
 
-		doc, err := goquery.NewDocument("https://en.wikipedia.org/wiki/Example.com")
-		
-		if err != nil {
-			log.Fatal(err)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	doc.Find("a[href]").Each(func(index int, item *goquery.Selection) {
+		href, _ := item.Attr("href")
+		fmt.Printf("link: %s - anchor text: %s\n", href, item.Text())
+		LINKS = append(LINKS, href)
+	})
+	for i := 0; i < len(LINKS); i++ {
+
+		url := fmt.Sprintf(LINKS[i])
+		if strings.Contains(url, "https") {
+			fmt.Printf("HTML code of %s ...\n", url)
+			resp, err := http.Get(url)
+			// handle the error if there is one
+			if err != nil {
+				panic(err)
+			}
+			// do this now so it won't be forgotten
+			defer resp.Body.Close()
+			// reads html as a slice of bytes
+			html, err := ioutil.ReadAll(resp.Body)
+			if err != nil {
+				panic(err)
+			}
+			// show the HTML code as a string %s
+			souphtml := fmt.Sprintf("%s\n", html)
+			foundemails(souphtml)
 		}
-		
-		doc.Find("a[href]").Each(func(index int, item *goquery.Selection) {
-			href, _ := item.Attr("href")
-			fmt.Printf("link: %s - anchor text: %s\n", href, item.Text())
-			
-		})
-		
-	
-	
-	foundemails(souphtml)
+	}
+
 }
 func foundemails(souphtml string) {
 	text := []byte(souphtml)
