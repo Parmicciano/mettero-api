c7f22a4112057dfb9117c7eb59fe743a8f1ee6d0
Parmicciano <85128225+Parmicciano@users.noreply.github.com>
2021-10-01 22:19:29 +0200

    wq

--- main.go
+++ main.go
@@ -21,90 +21,86 @@ type WebsiteData struct {
 }
 
 func main() {
-	resp, err := http.Get("http://35.180.242.58/getData/")
-	if err != nil {
-		fmt.Println("No response from request")
-	}
-
-	body, err := ioutil.ReadAll(resp.Body) // response body is []byte
-	monjson := fmt.Sprintf(string(body))
-	fmt.Printf(monjson)
-
-	var websiteData1 WebsiteData
-	erre := json.Unmarshal([]byte(monjson), &websiteData1)
-	if erre != nil {
-		fmt.Println(err)
-	}
-	fmt.Sprintf("Struct is:", websiteData1.Url)
-	url := fmt.Sprintf(websiteData1.Url)
-	userid := fmt.Sprintf(websiteData1.Userid)
-	dbid := fmt.Sprintf(websiteData1.Dbid)
-
-
-	fmt.Sprintf(userid + dbid)
-	defer resp.Body.Close()
-
-	LINKS := []string{}
-
-	doc, err := goquery.NewDocument(url)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	doc.Find("a[href]").Each(func(index int, item *goquery.Selection) {
-		href, _ := item.Attr("href")
-		fmt.Printf("link: %s - anchor text: %s\n", href, item.Text())
-
-		re := regexp.MustCompile(`^(?:https?:\/\/)?(?:[^@\/\n]+@)?(?:www\.)?([^:\/\n]+)`)
-
-		submatchall := re.FindAllString(href, -1)
-		for _, element := range submatchall {
-			domainame := fmt.Sprintf(element)
-
-			if strings.Contains(href, "https") == false {
-				href = domainame + href
-
-			}
+	for true {
+		resp, err := http.Get("http://35.180.242.58/getData/")
+		if err != nil {
+			fmt.Println("No response from request")
 		}
-		LINKS = append(LINKS, href)
-	})
-	for i := 0; i < len(LINKS); i++ {
 
-		url := fmt.Sprintf(LINKS[i])
+		body, err := ioutil.ReadAll(resp.Body) // response body is []byte
+		monjson := fmt.Sprintf(string(body))
+		fmt.Printf(monjson)
 
-		if strings.Contains(url, "https") {
-			fmt.Printf("HTML code of %s ...\n", url)
-			resp, err := http.Get(url)
-			// handle the error if there is one
-			if err != nil {
-				panic(err)
-			}
-			// do this now so it won't be forgotten
-			defer resp.Body.Close()
-			// reads html as a slice of bytes
-			html, err := ioutil.ReadAll(resp.Body)
-			if err != nil {
-				panic(err)
-			}
-			// show the HTML code as a string %s
-			souphtml := fmt.Sprintf("%s\n", html)
-			text := []byte(souphtml)
-			validateHost := false
-			emails := emailaddress.Find(text, validateHost)
+		var websiteData1 WebsiteData
+		erre := json.Unmarshal([]byte(monjson), &websiteData1)
+		if erre != nil {
+			fmt.Println(err)
+		}
+		fmt.Sprintf("Struct is:", websiteData1.Url)
+		url := fmt.Sprintf(websiteData1.Url)
+		userid := fmt.Sprintf(websiteData1.Userid)
+		dbid := fmt.Sprintf(websiteData1.Dbid)
+		fmt.Sprintf(userid + dbid)
+		defer resp.Body.Close()
 
-			for _, e := range emails {
-				fmt.Println(e)
-				var emailfound string = fmt.Sprintf("%s", e)
-				if !strings.Contains(emailfound, "png") && !strings.Contains(emailfound, "jpg") {
-					http.Get("http://35.180.242.58/getData/")
-					
+		LINKS := []string{}
+
+		doc, err := goquery.NewDocument(string(url))
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		doc.Find("a[href]").Each(func(index int, item *goquery.Selection) {
+			href, _ := item.Attr("href")
+			fmt.Printf("link: %s - anchor text: %s\n", href, item.Text())
+
+			re := regexp.MustCompile(`^(?:https?:\/\/)?(?:[^@\/\n]+@)?(?:www\.)?([^:\/\n]+)`)
+
+			submatchall := re.FindAllString(href, -1)
+			for _, element := range submatchall {
+				domainame := fmt.Sprintf(element)
+
+				if strings.Contains(href, "https") == false {
+					href = domainame + href
 
 				}
 			}
+			LINKS = append(LINKS, href)
+		})
+		for i := 0; i < len(LINKS); i++ {
 
-		
+			url := fmt.Sprintf(LINKS[i])
 
+			if strings.Contains(url, "https://") {
+				fmt.Printf("HTML code of %s ...\n", url)
+				resp, err := http.Get(url)
+				// handle the error if there is one
+				if err != nil {
+					panic(err)
+				}
+				// do this now so it won't be forgotten
+				defer resp.Body.Close()
+				// reads html as a slice of bytes
+				html, err := ioutil.ReadAll(resp.Body)
+				if err != nil {
+					panic(err)
+				}
+				// show the HTML code as a string %s
+				souphtml := fmt.Sprintf("%s\n", html)
+				text := []byte(souphtml)
+				validateHost := false
+				emails := emailaddress.Find(text, validateHost)
+
+				for _, e := range emails {
+					fmt.Println(e)
+					var emailfound string = fmt.Sprintf("%s", e)
+					if !strings.Contains(emailfound, "png") && !strings.Contains(emailfound, "jpg") {
+						http.Get("http://35.180.242.58/getData/?email=" + emailfound + "&userid=" + userid + "&dbid=" + dbid)
+
+					}
+				}
+
+			}
 		}
 	}
-
 }
